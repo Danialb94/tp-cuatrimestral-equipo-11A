7ae7a16445e7e01a29a676758ab2d09faeda963c
AbrildelaIglesia <abixdlie@gmail.com>
2025-11-05 23:23:37 -0300

    Agrego mod. y baja al negocio de pacientes, Modifico las altas

--- negocio/PacienteNegocio.cs
+++ negocio/PacienteNegocio.cs
@@ -10,7 +10,7 @@ namespace negocio
 {
     internal class PacienteNegocio
     {
-        public List<Paciente> listar()
+        public List<Paciente> Listar()
         {
             List<Paciente> lista = new List<Paciente>();
             AccesoDatos datos = new AccesoDatos();
@@ -23,25 +23,19 @@ namespace negocio
                 {
                     Paciente paciente = new Paciente();
                     paciente.IdPaciente = (int)datos.Lector["IdPaciente"];
-                    if (!(datos.Lector["Nombre"] is DBNull))
-                        paciente.Nombre = (string)datos.Lector["Nombre"];
-                    if (!(datos.Lector["Apellido"] is DBNull))
-                        paciente.Apellido = (string)datos.Lector["Apellido"];
-                    if (!(datos.Lector["TipoDocumento"] is DBNull))
-                    {
-                        paciente.TipoDocumento.Descripcion = (string)datos.Lector["TipoDocumento"];
-                        paciente.TipoDocumento.IdTipoDocumento = (int)datos.Lector["IdTipoDocumento"];
-                    }
-                    if (!(datos.Lector["Documento"] is DBNull))
-                        paciente.Documento = (string)datos.Lector["Documento"];
-                    if (!(datos.Lector["Domicilio"] is DBNull))
-                        paciente.Domicilio = (string)datos.Lector["Domicilio"];
-                    if (!(datos.Lector["FechaNacimiento"] is DBNull))
-                        paciente.FechaNacimiento = (DateTime)datos.Lector["FechaNacimiento"];
+                    paciente.Nombre = (string)datos.Lector["Nombre"];
+                    paciente.Apellido = (string)datos.Lector["Apellido"];
+                    if (!(datos.Lector["Telefono"] is DBNull))
+                        paciente.Telefono = (string)datos.Lector["Telefono"];
+                    paciente.TipoDocumento.IdTipoDocumento = (int)datos.Lector["IdTipoDocumento"];
+                    paciente.TipoDocumento.Descripcion = (string)datos.Lector["TipoDocumento"];
+                    paciente.Documento = (string)datos.Lector["Documento"];
+                    paciente.Domicilio = (string)datos.Lector["Domicilio"];
+                    paciente.FechaNacimiento = (DateTime)datos.Lector["FechaNacimiento"];
                     if (!(datos.Lector["Cobertura"] is DBNull))
                     {
-                        paciente.Cobertura.Descripcion = (string)datos.Lector["Cobertura"];
                         paciente.Cobertura.IdCobertura = (int)datos.Lector["IdCobertura"];
+                        paciente.Cobertura.Descripcion = (string)datos.Lector["Cobertura"];
                     }
                     if (!(datos.Lector["IdImagen"] is DBNull))
                     {
@@ -64,7 +58,7 @@ namespace negocio
             }
         }
 
-        public void agregar(Paciente nuevo)
+        public void Agregar(Paciente nuevo)
         {
             AccesoDatos datos = new AccesoDatos();
             try
@@ -104,6 +98,67 @@ namespace negocio
             }
         }
 
+        public void Modificar(Paciente nuevo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("UPDATE Pacientes SET IdTipoDocumento = @IdTipoDocumento, Documento = @Documento, Domicilio = @Domicilio, FechaNacimiento = @FechaNacimiento, IdCobertura = @IdCobertura WHERE IdPaciente = @IdPaciente; go UPDATE Personas  SET Nombre = @Nombre, Apellido = @Apellido, Telefono = @Telefono WHERE IdPersona = @IdPersona; go UPDATE Usuarios  SET Email = @Email, Contrasenia = @Contrasenia IdPermiso = @IdPermiso WHERE IdUsuario = @IdUsuario;");
+                //USUARIO
+                datos.setearParametro("@IdPaciente", nuevo.IdPaciente);
+                datos.setearParametro("@Email", nuevo.Usuario.Email);
+                datos.setearParametro("@Contrasenia", nuevo.Usuario.Contrasenia);
+                datos.setearParametro("@IdPermiso", nuevo.Usuario.Permiso.IdPermiso);
+                //PERSONA
+                datos.setearParametro("@IdPersona", nuevo.IdPersona);
+                datos.setearParametro("@Nombre", nuevo.Nombre);
+                datos.setearParametro("@Apellido", nuevo.Apellido);
+                datos.setearParametro("@Telefono", nuevo.Telefono);
+                //PACIENTES
+                datos.setearParametro("@IdUsuario", nuevo.IdUsuario);
+                datos.setearParametro("@IdTipoDocumento", nuevo.TipoDocumento.IdTipoDocumento);
+                datos.setearParametro("@Documento", nuevo.Documento);
+                datos.setearParametro("@Domicilio", nuevo.Domicilio);
+                datos.setearParametro("@FechaNacimiento", nuevo.FechaNacimiento);
+                datos.setearParametro("@IdCobertura", nuevo.Cobertura.IdCobertura);
+                datos.ejecutarLectura();
 
+                // INSERT TABLA IMAGENES
+                if (nuevo.Imagen.UrlImagen != null)
+                {
+                    ImagenNegocio imgNegocio = new ImagenNegocio();
+                    imgNegocio.Modificar(nuevo.Imagen);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void Baja(int IdPaciente)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("UPDATE Usuarios SET Estado = 0 WHERE IdUsuario = (SELECT IdUsuario FROM Personas WHERE IdPersona = (SELECT IdPersona FROM Pacientes WHERE IdPaciente = @IdPaciente))");
+                //USUARIO
+                datos.setearParametro("@IdPaciente", IdPaciente);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
     }
 }
